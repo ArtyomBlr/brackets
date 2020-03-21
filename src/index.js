5e2e729bef896f70cc811495a6fcf71d92ffbd7e
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-03-21 12:20:38 +0300

    feat: task is ready!

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  let pattern = bracketsConfig.map(el => {
-    el.join('')
-  });
+
+  let pattern = bracketsConfig.map(el => el.join(''));
 
   for(let i = 0; i < pattern.length; i++){
-    if(str.includes(pattern[i])){
+    while(str.includes(pattern[i])){
       str = str.replace(pattern[i], '');
-
+      i = -1;
     }
   }
-
   return str.length == 0 ? true : false;
-
-  //return pattern
 }
