c5e12bb8710f31323bf91f2c6c21c2d02a2986ed
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-03-21 12:01:20 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pattern = bracketsConfig.map(el => {
+    el.join('')
+  });
+
+  for(let i = 0; i < pattern.length; i++){
+    if(str.includes(pattern[i])){
+      str = str.replace(pattern[i], '');
+
+    }
+  }
+
+  return str.length == 0 ? true : false;
+
+  //return pattern
 }
